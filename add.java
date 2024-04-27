e83e396ca8d5188de15173338ac7da4efa4f795f
Sarvesh-2103 <sar.mha1.rt21@dypatil.edu>
2024-04-27 13:58:42 +0530

    initial commit

---
+++ add.java
@@ -0,0 +1,8 @@
+
+public class add {
+    public static void main(String[] args) {
+        int num1 = 5;  // Example number
+        int num2 = 10; // Example number
+        System.out.println("Sum = " + (num1 + num2));
+    }
+}
